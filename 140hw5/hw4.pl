015cc1a6beb4fa1b4211622edfe8366516ad97cb
Yiming(Mike) Peng <69832503+MacPeng@users.noreply.github.com>
2021-07-23 23:48:27 -0700

    Update hw4.pl

--- 140hw5/hw4.pl
+++ 140hw5/hw4.pl
@@ -6,12 +6,6 @@ shuffle([X|Xs], [Y|Ys], [X,Y|Z]) :- shuffle(Xs,Ys,Z).
 
 
 
-%question 3
-members(X, [X|_]).
-members(X, [_|T]):- members(X,T).
 
-no_duplicates([Y|[]],[Y|[]]).
-no_duplicates([A|B],C) :- members(A,B), no_duplicates(B,C).
-no_duplicates([D|E],[D|[F|G]]) :-  \+ members(D,E), no_duplicates(E,[F|G]).
 
 
