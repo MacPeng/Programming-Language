147129d445e9cbe4d79fcf309e4b2522b46ced4c
Yiming(Mike) Peng <69832503+MacPeng@users.noreply.github.com>
2021-07-28 09:48:04 -0700

    Update hw4.pl

--- 140hw5/hw4.pl
+++ 140hw5/hw4.pl
@@ -8,8 +8,3 @@ no_duplicates([Y|[]],[Y|[]]).
 no_duplicates([A|B],C) :- members(A,B), no_duplicates(B,C).
 no_duplicates([D|E],[D|[F|G]]) :-  \+ members(D,E), no_duplicates(E,[F|G]).
 
-
-
-% question 4
-deletes(X,[X|Xs],Xs).
-deletes(X,[Y|Ys],[Y|Z]) :- deletes(X,Ys,Z).
