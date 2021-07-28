f5eaf35c99099a82dfb231db672f8d5ff1acbf92
Yiming(Mike) Peng <69832503+MacPeng@users.noreply.github.com>
2021-07-28 09:47:50 -0700

    Update hw4.pl

--- 140hw5/hw4.pl
+++ 140hw5/hw4.pl
@@ -1,3 +1,15 @@
 same_elements([],[]).
 same_elements([X|Xs],Ys) :- same_elements(Xs,Zs), deletes(X,Ys,Zs).
 
+
+
+
+no_duplicates([Y|[]],[Y|[]]).
+no_duplicates([A|B],C) :- members(A,B), no_duplicates(B,C).
+no_duplicates([D|E],[D|[F|G]]) :-  \+ members(D,E), no_duplicates(E,[F|G]).
+
+
+
+% question 4
+deletes(X,[X|Xs],Xs).
+deletes(X,[Y|Ys],[Y|Z]) :- deletes(X,Ys,Z).
